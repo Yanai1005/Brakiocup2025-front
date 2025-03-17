249b83b7534985c4ace41cb07b0fd654e058094f
NazonoKansatugata <0gy27b297263j6s@au.com>
2025-03-17 12:04:48 +0900

    ちょっと画面直す

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -15,9 +15,9 @@ const About = () => {
     grade = 'A';
   } else if (score >= 80) {
     grade = 'B';
-  } else if (score >= 70) {
-    grade = 'C';
   } else if (score >= 60) {
+    grade = 'C';
+  } else if (score >= 40) {
     grade = 'D';
   } else {
     grade = 'E';
